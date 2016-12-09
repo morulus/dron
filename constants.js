aa1bb50e5c757cec65d8d7f5be1a0accb0fc3cfd
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-12-09 13:29:34 +0300

    changes to erector

---
+++ constants.js
@@ -0,0 +1,9 @@
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.ENV = Symbol('ENV');
+exports.ACTION_SET_STATE = Symbol('ACTION_SET_STATE');
+exports.ACTION_ASSIGN_REDUCER = Symbol('ACTION_ASSIGN_REDUCER');
+exports.ACTION_RUN = Symbol('ACTION_RUN');
+exports.LAST_REDUCER = Symbol('LAST_REDUCER');
