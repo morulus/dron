d6e9a23f407a8c799a8c0f39256083e8bad7fcaa
Vladimir Kalmykov <vladimirmorulus@gmail.com>
2017-01-22 23:54:50 +0300

    needed

--- index.js
+++
@@ -1,2 +0,0 @@
-exports.runModule = require('./lib/runModule.js');
-exports.runPackage = require('./lib/runPackage.js');
--- tests/resolvePackage.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const packageName = process.argv[2];
-const resolvePackage = require('./../lib/resolvePackage.js');
-const result = resolvePackage(packageName);
-console.log('result', result);
